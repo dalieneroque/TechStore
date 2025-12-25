60b641dd3eb6e880d7c39061c1dc73e4941fca39
Daliene Nonato Lima Roque <dalienenl@gmail.com>
2025-12-25 14:58:41 -0300

    feat: adiciona interface IProdutoRepository com métodos assíncronos
    - Criada a interface IProdutoRepository herdando de IRepository<Produto>
    - Adicionados métodos para:
    - Buscar produtos ativos
    - Buscar produtos por categoria
    - Buscar produtos com estoque
    - Buscar produto com categoria
    - Pesquisa de produtos por termo

---
+++ src/TechStore.Core/Interfaces/IProdutoRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using TechStore.Core.Entities;
+
+namespace TechStore.Core.Interfaces
+{
+    public interface IProdutoRepository : IRepository<Produto>
+    {
+        // Métodos específicos para Produto
+        Task<IEnumerable<Produto>> GetProdutosAtivosAsync();
+        Task<IEnumerable<Produto>> GetProdutosPorCategoriaAsync(int categoriaId);
+        Task<IEnumerable<Produto>> GetProdutoComEstoqueAsync();
+        Task<Produto> GetProdutoComCategoriaAsync(int id);
+        Task<IEnumerable<Produto>> SearchProdutosAsync(string searchTerm);
+
+    }
+}
