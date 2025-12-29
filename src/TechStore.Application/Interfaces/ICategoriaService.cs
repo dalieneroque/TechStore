59d61c0a5782eccc237bc0945e3db28eed3a554b
Daliene Nonato Lima Roque <dalienenl@gmail.com>
2025-12-29 19:08:34 -0300

    feat: adiciona interfaces de serviços de categoria e produto

---
+++ src/TechStore.Application/Interfaces/ICategoriaService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TechStore.Application.DTOs;
+
+namespace TechStore.Application.Interfaces
+{
+    public interface ICategoriaService
+    {
+        Task<IEnumerable<CategoriaDTO>> ObterTodasCategoriasAsync();
+        Task<IEnumerable<CategoriaDTO>> ObterCategoriasAtivasAsync();
+        Task<CategoriaDTO> ObterCategoriaPorIdAsync(int id);
+        Task<CategoriaDTO> CriarCategoriaAsync(CriarCategoriaDTO categoriaDTO);
+        Task AtualizarCategoriaAsync(int id, AtualizarCategoriaDTO categoriaDTO);
+        Task ExcluirCategoriaAsync(int id);
+        Task<bool> CategoriaExisteAsync(int id);
+    }
+}
---
+++ src/TechStore.Application/Interfaces/IProdutoService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TechStore.Application.DTOs;
+
+namespace TechStore.Application.Interfaces
+{
+    public interface IProdutoService
+    {
+        Task<IEnumerable<ProdutoDTO>> ObterTodosProdutosAsync();
+        Task<IEnumerable<ProdutoDTO>> ObterProdutosAtivosAsync();
+        Task<IEnumerable<ProdutoDTO>> ObterProdutosPorCategoriaAsync(int categoriaId);
+        Task<IEnumerable<ProdutoDTO>> ObterProdutosComEstoqueAsync();
+        Task<ProdutoDTO> ObterProdutoPorIdAsync(int id);
+        Task<ProdutoDTO> CriarProdutoAsync(CriarProdutoDTO produtoDTO);
+        Task AtualizarProdutoAsync(int id, AtualizarProdutoDTO produtoDTO);
+        Task AtualizarEstoqueAsync(int id, AtualizarEstoqueDTO estoqueDTO);
+        Task ExcluirProdutoAsync(int id);
+        Task<IEnumerable<ProdutoDTO>> BuscarProdutosAsync(string termo);
+
+        // Novos métodos específicos
+        Task<IEnumerable<ProdutoDTO>> ObterProdutosRecentesAsync(int quantidade);
+        Task<IEnumerable<ProdutoDTO>> ObterProdutosEmPromocaoAsync();
+    }
+}
