6ccf14ae8a3896c2b090456c73eb45aa90677251
Daliene Nonato Lima Roque <dalienenl@gmail.com>
2026-01-16 21:33:58 -0300

    feat: Adiciona repositórios para avaliações e favoritos

---
+++ src/TechStore.Infrastructure/Repositories/AvaliacaoRepository.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechStore.Core.Entities;
+using TechStore.Core.Interfaces;
+using TechStore.Infrastructure.Data;
+
+namespace TechStore.Infrastructure.Repositories
+{
+    public class AvaliacaoRepository : Repository<Avaliacao>, IAvaliacaoRepository
+    {
+        public AvaliacaoRepository(TechStoreDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Avaliacao>> ObterAvaliacoesPorProdutoAsync(int produtoId, bool apenasAprovadas = true)
+        {
+            var query = _context.Avaliacoes
+                .Include(a => a.Usuario)
+                .Where(a => a.ProdutoId == produtoId);
+
+            if (apenasAprovadas)
+                query = query.Where(a => a.Aprovada);
+
+            return await query
+                .OrderByDescending(a => a.DataAvaliacao)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Avaliacao>> ObterAvaliacoesPorUsuarioAsync(string usuarioId)
+        {
+            return await _context.Avaliacoes
+                .Include(a => a.Produto)
+                .Where(a => a.UsuarioId == usuarioId)
+                .OrderByDescending(a => a.DataAvaliacao)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Avaliacao>> ObterAvaliacoesPendentesAsync()
+        {
+            return await _context.Avaliacoes
+                .Include(a => a.Produto)
+                .Include(a => a.Usuario)
+                .Where(a => !a.Aprovada)
+                .OrderBy(a => a.DataAvaliacao)
+                .ToListAsync();
+        }
+
+        public async Task<decimal> ObterMediaAvaliacoesPorProdutoAsync(int produtoId)
+        {
+            var media = await _context.Avaliacoes
+                .Where(a => a.ProdutoId == produtoId && a.Aprovada)
+                .AverageAsync(a => (decimal?)a.Nota);
+
+            return media ?? 0;
+        }
+
+        public async Task<int> ContarAvaliacoesPorProdutoAsync(int produtoId)
+        {
+            return await _context.Avaliacoes
+                .CountAsync(a => a.ProdutoId == produtoId && a.Aprovada);
+        }
+    }
+}
---
+++ src/TechStore.Infrastructure/Repositories/FavoritoRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechStore.Core.Entities;
+using TechStore.Core.Interfaces;
+using TechStore.Infrastructure.Data;
+
+namespace TechStore.Infrastructure.Repositories
+{
+    public class FavoritoRepository : Repository<Favorito>, IFavoritoRepository
+    {
+        public FavoritoRepository(TechStoreDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Favorito>> ObterFavoritosPorUsuarioAsync(string usuarioId)
+        {
+            return await _context.Favoritos
+                .Include(f => f.Produto)
+                    .ThenInclude(p => p.Categoria)
+                .Where(f => f.UsuarioId == usuarioId)
+                .OrderByDescending(f => f.DataAdicao)
+                .ToListAsync();
+        }
+
+        public async Task<bool> ProdutoEstaFavoritadoAsync(string usuarioId, int produtoId)
+        {
+            return await _context.Favoritos
+                .AnyAsync(f => f.UsuarioId == usuarioId && f.ProdutoId == produtoId);
+        }
+
+        public async Task<bool> RemoverFavoritoAsync(string usuarioId, int produtoId)
+        {
+            var favorito = await _context.Favoritos
+                .FirstOrDefaultAsync(f => f.UsuarioId == usuarioId && f.ProdutoId == produtoId);
+
+            if (favorito != null)
+            {
+                _context.Favoritos.Remove(favorito);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
