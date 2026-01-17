a700c88d72ae95959e8946e5e700bca3abfa68a4
Daliene Nonato Lima Roque <dalienenl@gmail.com>
2026-01-16 21:27:09 -0300

    feat: Adicionar interfaces de avaliação e favorito ao repositório

---
+++ src/TechStore.Core/Interfaces/IAvaliacaoRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TechStore.Core.Entities;
+
+namespace TechStore.Core.Interfaces
+{
+    public interface IAvaliacaoRepository : IRepository<Avaliacao>
+    {
+        Task<IEnumerable<Avaliacao>> ObterAvaliacoesPorProdutoAsync(int produtoId, bool apenasAprovadas = true);
+        Task<IEnumerable<Avaliacao>> ObterAvaliacoesPorUsuarioAsync(string usuarioId);
+        Task<IEnumerable<Avaliacao>> ObterAvaliacoesPendentesAsync();
+        Task<decimal> ObterMediaAvaliacoesPorProdutoAsync(int produtoId);
+        Task<int> ContarAvaliacoesPorProdutoAsync(int produtoId);
+    }
+}
---
+++ src/TechStore.Core/Interfaces/IFavoritoRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TechStore.Core.Entities;
+
+namespace TechStore.Core.Interfaces
+{
+    public interface IFavoritoRepository : IRepository<Favorito>
+    {
+        Task<IEnumerable<Favorito>> ObterFavoritosPorUsuarioAsync(string usuarioId);
+        Task<bool> ProdutoEstaFavoritadoAsync(string usuarioId, int produtoId);
+        Task<bool> RemoverFavoritoAsync(string usuarioId, int produtoId);
+    }
+}
