8652535e93da0fd154426cc7e5cc7085e7a6ff8d
Daliene Nonato Lima Roque <dalienenl@gmail.com>
2026-02-05 17:53:14 -0300

    feat: criando a tela de login no blazor.

--- TechStore.Web/Program.cs
+++ TechStore.Web/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TechStore.Web;
 using Blazored.LocalStorage;
+using TechStore.Web.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,8 +10,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient
 {
-    BaseAddress = new Uri("https://localhost:7255")
+    BaseAddress = new Uri("https://localhost:7255/")
 });
 builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<ProdutoService>();
+builder.Services.AddScoped<CarrinhoService>();
+
 
 await builder.Build().RunAsync();
