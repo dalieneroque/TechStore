01f985b2ea4cc58e962c035fbaa9341d4a6af084
Daliene Nonato Lima Roque <dalienenl@gmail.com>
2026-04-04 21:38:53 -0300

    fix: adicionar migration para tornar Descricao de Categoria anulável

--- src/TechStore.Infrastructure/Migrations/20260405003806_AlterCategoriaDescricaoNullable.Designer.cs
+++ src/TechStore.Infrastructure/Migrations/20260405003806_AlterCategoriaDescricaoNullable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TechStore.Infrastructure.Data;
@@ -11,9 +12,11 @@ using TechStore.Infrastructure.Data;
 namespace TechStore.Infrastructure.Migrations
 {
     [DbContext(typeof(TechStoreDbContext))]
-    partial class TechStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260405003806_AlterCategoriaDescricaoNullable")]
+    partial class AlterCategoriaDescricaoNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
